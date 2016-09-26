40db307ea571bf04e472872e07e20ebf717a80ca
vasyoid <vasyoid@gmail.com>
2016-09-26 08:30:36 +0300

    improved code style and corrected mistakes

--- task-02/task-02.py
+++ task-02/task-02.py
@@ -2,40 +2,42 @@ import os
 import hashlib
 import time
 import argparse
+from collections import defaultdict
 
-equal_files = {}
 
 def walk_through(rootdir):
+	equal_files = defaultdict(list)
 	omit = len(rootdir) + 1
 	for subdir, dirs, files in os.walk(rootdir):
-		first_char = subdir.split('/')[-1][0]
-		dirs[:] = [d for d in dirs if not d[0] == '.' and not d[0] == '~']
+		dirs[:] = [d for d in dirs if not os.path.islink(d)]
 		for file in files:
-			file = subdir + '/' + file
-			hash = get_hash(file)
-			if equal_files.__contains__(hash):
-				equal_files[hash] += (file[omit:], )
-			else:
-				equal_files.update({hash: (file[omit:], )})
+			if not file[0] == '.' and not file[0] == '~' and not os.path.islink(file):
+				file = os.path.join(subdir, file)
+				hash = get_hash(file)
+				equal_files[hash].append(file[omit:])
+	return equal_files
+
 
 def get_hash(file):
-	f = open(file, 'rt')
-	sha1 = hashlib.sha1()
-	while True:
-		part = f.read(50000)
-		sha1.update(part.encode("utf-8"))
-		if len(part) == 0:
-			break
-	f.close()
+	with open(file, 'rb') as f:
+		sha1 = hashlib.sha1()
+		while True:
+			part = f.read(50000)
+			sha1.update(part)
+			if not len(part):
+				break
 	return sha1.hexdigest()
 
+
 def main():
-	parser = argparse.ArgumentParser(description="output files with equal content")
+	parser = argparse.ArgumentParser(
+	description="output files with equal content")
 	parser.add_argument("path", type=str, help="home directory")
 	args = parser.parse_args()
-	walk_through(args.path)
-	for file in equal_files:
-		print(':'.join(equal_files[file]) + '\n' if len(equal_files[file]) > 1 else "", end = "")
+	equal_files = walk_through(args.path)
+	for value in equal_files.values():
+		if len(value) > 1:
+			print(':'.join(value))
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 main()
