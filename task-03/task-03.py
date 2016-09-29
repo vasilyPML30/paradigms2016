2b0436be0fc6f763bf2de708fd3a7b148ceb0da3
vasyoid <vasyoid@gmail.com>
2016-09-29 22:51:33 +0300

    corrected code style

--- task-03/task-03.py
+++ task-03/task-03.py
@@ -1,22 +1,19 @@
 import math
 import numpy
+import sys
 
 
 def read(real_size, size):
-    matr = numpy.zeros(shape=(size, size), dtype=int)
-    for i in range(real_size):
-        j = 0
-        for n in input().split(' '):
-            matr[i][j] = n
-            j += 1
+    matr = numpy.zeros(shape=(size << 1, size), dtype=int)
+    inpt = numpy.loadtxt(sys.stdin, ndmin=2)
+    matr[:real_size, :real_size] = inpt[:real_size]
+    matr[size:size + real_size, :real_size] = inpt[:real_size]
     return matr
 
 
 def write(matr, size):
-    for i in range(size):
-        for j in range(size):
-            print(matr[i][j], end=" ")
-        print()
+    for row in matr[:size, :size]:
+        print(*row)
 
 
 def mult(M1, M2):
@@ -27,39 +24,44 @@ def mult(M1, M2):
         m1[0][0] *= m2[0][0]
         return m1
     size >>= 1
-    A = numpy.ndarray(shape=(2, 2, size, size), dtype=int)
-    B = numpy.ndarray(shape=(2, 2, size, size), dtype=int)
-    C = numpy.ndarray(shape=(2, 2, size, size), dtype=int)
-    for i in range(2):
-        for j in range(2):
-            A[i][j] = m1[size * i:size * (i + 1), size * j:size * (j + 1)]
-    for i in range(2):
-        for j in range(2):
-            B[i][j] = m2[size * i:size * (i + 1), size * j:size * (j + 1)]
-    P1 = mult(A[0][0] + A[1][1], B[0][0] + B[1][1])
-    P2 = mult(A[1][0] + A[1][1], B[0][0])
-    P3 = mult(A[0][0], B[0][1] - B[1][1])
-    P4 = mult(A[1][1], B[1][0] - B[0][0])
-    P5 = mult(A[0][0] + A[0][1], B[1][1])
-    P6 = mult(A[1][0] - A[0][0], B[0][0] + B[0][1])
-    P7 = mult(A[0][1] - A[1][1], B[1][0] + B[1][1])
-    C[0][0] = P1 + P4 + P7 - P5
-    C[0][1] = P3 + P5
-    C[1][0] = P2 + P4
-    C[1][1] = P1 + P3 + P6 - P2
-    ans = numpy.ndarray(shape=(size << 1, size << 1), dtype=int)
-    for i in range(2):
-        for j in range(2):
-            ans[size * i:size * (i + 1), size * j:size * (j + 1)] = C[i][j]
-    return ans
+    A00 = m1[:size, :size]
+    A01 = m1[:size, size:]
+    A10 = m1[size:, :size]
+    A11 = m1[size:, size:]
+
+    B00 = m2[:size, :size]
+    B01 = m2[:size, size:]
+    B10 = m2[size:, :size]
+    B11 = m2[size:, size:]
+
+    P1 = mult(A00 + A11, B00 + B11)
+    P2 = mult(A10 + A11, B00)
+    P3 = mult(A00, B01 - B11)
+    P4 = mult(A11, B10 - B00)
+    P5 = mult(A00 + A01, B11)
+    P6 = mult(A10 - A00, B00 + B01)
+    P7 = mult(A01 - A11, B10 + B11)
+
+    C00 = P1 + P4 + P7 - P5
+    C01 = P3 + P5
+    C10 = P2 + P4
+    C11 = P1 + P3 + P6 - P2
+
+    ANS = numpy.zeros(shape=(size << 1, size << 1), dtype=int)
+
+    ANS[:size, :size] = C00
+    ANS[:size, size:] = C01
+    ANS[size:, :size] = C10
+    ANS[size:, size:] = C11
+
+    return ANS
 
 
 def main():
     real_size = int(input())
     size = 1 << math.ceil(math.log2(real_size))
-    matr1 = read(real_size, size)
-    matr2 = read(real_size, size)
-    write(mult(matr1, matr2), real_size)
+    matr = read(real_size, size)
+    write(mult(matr[:size], matr[size:]), real_size)
 
 
 if __name__ == "__main__":
