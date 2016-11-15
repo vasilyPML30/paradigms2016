fb621748b32b6bafb51fa3fa6fcbf9e568742d25
vasyoid <vasyoid@gmail.com>
2016-11-15 23:40:55 +0300

    fixed: standard functions are not used

--- task-08/Fp1.hs
+++ task-08/Fp1.hs
@@ -16,27 +16,18 @@ drop' 0 l = l
 drop' n (x : xs) = drop' (n - 1) xs
 
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' f xs = [x | x <- xs, f x == True]
+filter' f [] = []
+filter' f (x : xs) | f x == True = x : (filter' f xs)
+                   | otherwise = filter' f xs
 
 foldl' :: (a -> b -> a) -> a -> [b] -> a
 foldl' f z [] = z
 foldl' f z (x : xs) = foldl' f (f z x) xs
 
 concat' :: [a] -> [a] -> [a]
-concat' a b = a ++ b
-
-filter'' :: (a -> a -> Bool) -> a -> [a] -> [a]
-filter'' f a xs = [x | x <- xs, f x a == True]
-
-less' :: Ord a => a -> a -> Bool
-less' a b = a < b
-
-more' :: Ord a => a -> a -> Bool
-more' a b = a > b
-
-eq' :: Ord a => a -> a -> Bool
-eq' a b = a == b
+concat' [] b = b
+concat' (x : xs) b = x : (concat' xs b)
 
 quickSort' :: Ord a => [a] -> [a]
 quickSort' [] = []
-quickSort' (x : xs) = (quickSort' (filter'' less' x xs) ++ filter'' eq' x ([x] ++ xs)) ++ quickSort' (filter'' more' x xs)
+quickSort' (x : xs) = concat' (concat' (quickSort' (filter' (< x) xs)) (filter' (== x) (concat' [x] xs))) (quickSort' (filter' (> x) xs))
